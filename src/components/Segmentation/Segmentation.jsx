01bb0fa504b46d9a44ab6d864c7148f04100068b
Marina <dubinamarina777@gmail.com>
2023-07-04 13:15:31 +0300

    add segmentation component

--- src/components/Segmentation/Segmentation.jsx
+++ src/components/Segmentation/Segmentation.jsx
@@ -1,26 +1,88 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./segmentation.module.scss";
-import SimpleSelect from "../SimpleSelect/SimpleSelect";
+import SegmentationItem from "./SegmentationItem";
 
 const Segmentation = () => {
+    const [segmentationOpen, setSegmentationOpen] = useState(false);
+    const [segmentationItems, setSegmentationItems] = useState([]);
+
+    const handleSegmentation = () => {
+        setSegmentationOpen(!segmentationOpen)
+        addSegmentationItem()
+    }
+
+    // Function to add a SegmentationItem
+    const addSegmentationItem = () => {
+        setSegmentationItems([...segmentationItems, {}]);
+    };
+
+    // Function to remove a SegmentationItem
+    const removeSegmentationItem = (index) => {
+        const updatedItems = segmentationItems.filter((item, i) => i !== index);
+        if (segmentationItems.length === 1) {
+            setSegmentationOpen(false)
+        }
+        setSegmentationItems(updatedItems);
+    };
+
+    // Function to remove all segmentations
+    const removeAllSegmentations = () => {
+        setSegmentationItems([]);
+        setSegmentationOpen(false)
+    };
 
     return (
         <div className={styles.container}>
-            <div className={styles.addSeg}>
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
-                     xmlns="http://www.w3.org/2000/svg">
-                    <path d="M6 12H18" stroke="#F0B11C" strokeWidth="1.5" strokeLinecap="round"
-                          strokeLinejoin="round"/>
-                    <path d="M12 6V18" stroke="#F0B11C" strokeWidth="1.5" strokeLinecap="round"
-                          strokeLinejoin="round"/>
-                </svg>
-                Добавить сегментацию
-            </div>
-            <div className={styles.segmentationWrapper}>
-                <div className={styles.item}>
-                    <SimpleSelect label={""} placeholder={"Был внесен дипозит"} isMulti={false}/>
+            {!segmentationOpen ?
+                <div className={styles.addSeg} onClick={handleSegmentation}>
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                         xmlns="http://www.w3.org/2000/svg">
+                        <path d="M6 12H18" stroke="#F0B11C" strokeWidth="1.5" strokeLinecap="round"
+                              strokeLinejoin="round"/>
+                        <path d="M12 6V18" stroke="#F0B11C" strokeWidth="1.5" strokeLinecap="round"
+                              strokeLinejoin="round"/>
+                    </svg>
+                    Добавить сегментацию
                 </div>
-            </div>
+                : <div>
+                    <h3 className={styles.opened}>
+                        Сегментация
+                        <svg width="20" height="20" viewBox="0 0 20 20" fill="none"
+                             xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M10 17.5C14.1421 17.5 17.5 14.1421 17.5 10C17.5 5.85786 14.1421 2.5 10 2.5C5.85786 2.5 2.5 5.85786 2.5 10C2.5 14.1421 5.85786 17.5 10 17.5Z"
+                                stroke="#F0B11C" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                            <path d="M9.375 9.375H10V13.75H10.625" stroke="#F0B11C" strokeWidth="1.5"
+                                  strokeLinecap="round" strokeLinejoin="round"/>
+                            <path
+                                d="M9.84375 7.5C10.3615 7.5 10.7812 7.08027 10.7812 6.5625C10.7812 6.04473 10.3615 5.625 9.84375 5.625C9.32598 5.625 8.90625 6.04473 8.90625 6.5625C8.90625 7.08027 9.32598 7.5 9.84375 7.5Z"
+                                fill="#F0B11C"/>
+                        </svg>
+                    </h3>
+                    <div className={styles.segmentationWrapper}>
+                        {segmentationItems.map((item, index) => (
+                            <SegmentationItem
+                                key={index}
+                                index={index}
+                                removeSegmentationItem={removeSegmentationItem}
+                            />
+                        ))}
+                        <div className={styles.actions}>
+                            <div className={styles.add} onClick={addSegmentationItem}>
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M6 12H18" stroke="#F0B11C" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                                    <path d="M12 6V18" stroke="#F0B11C" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                                </svg>
+                                Добавить сегментацию
+                            </div>
+                            <div className={styles.remove} onClick={removeAllSegmentations}>
+                                Очистить
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            }
+
         </div>
     );
 };
--- src/components/SimpleSelect/SimpleSelect.tsx
+++ src/components/SimpleSelect/SimpleSelect.tsx
@@ -7,6 +7,7 @@ interface SelectProps {
     value: string;
     label: string;
 }
+
 interface SimpleSelectProps {
     label: string;
     placeholder: string;
@@ -31,22 +32,32 @@ const SimpleSelect: React.FC<SimpleSelectProps> = ({label, placeholder, isMulti}
         <div className={styles.inputWrapper}>
             <div className={styles.select}>
                 {label}
-                <svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M10.5 17.5C14.6421 17.5 18 14.1421 18 10C18 5.85786 14.6421 2.5 10.5 2.5C6.35786 2.5 3 5.85786 3 10C3 14.1421 6.35786 17.5 10.5 17.5Z" stroke="#F0B11C" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                <path d="M9.875 9.375H10.5V13.75H11.125" stroke="#F0B11C" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-                <path d="M10.3438 7.5C10.8615 7.5 11.2812 7.08027 11.2812 6.5625C11.2812 6.04473 10.8615 5.625 10.3438 5.625C9.82598 5.625 9.40625 6.04473 9.40625 6.5625C9.40625 7.08027 9.82598 7.5 10.3438 7.5Z" fill="#F0B11C"/>
-            </svg>
+                {label !== "" ?
+                    <svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M10.5 17.5C14.6421 17.5 18 14.1421 18 10C18 5.85786 14.6421 2.5 10.5 2.5C6.35786 2.5 3 5.85786 3 10C3 14.1421 6.35786 17.5 10.5 17.5Z"
+                            stroke="#F0B11C" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                        <path d="M9.875 9.375H10.5V13.75H11.125" stroke="#F0B11C" strokeWidth="1.5"
+                              strokeLinecap="round" strokeLinejoin="round"/>
+                        <path
+                            d="M10.3438 7.5C10.8615 7.5 11.2812 7.08027 11.2812 6.5625C11.2812 6.04473 10.8615 5.625 10.3438 5.625C9.82598 5.625 9.40625 6.04473 9.40625 6.5625C9.40625 7.08027 9.82598 7.5 10.3438 7.5Z"
+                            fill="#F0B11C"/>
+                    </svg>
+                    :
+                    ""
+                }
+
             </div>
-        <Select
-            isMulti={isMulti}
-            className="react-select-container"
-            classNamePrefix="react-select"
-            onChange={(option: SingleValue<any>) => {
-                handleSelectChange(option)
-            }}
-            options={options}
-            placeholder={placeholder}
-        />
+            <Select
+                isMulti={isMulti}
+                className="react-select-container"
+                classNamePrefix="react-select"
+                onChange={(option: SingleValue<any>) => {
+                    handleSelectChange(option)
+                }}
+                options={options}
+                placeholder={placeholder}
+            />
         </div>
     );
 };
